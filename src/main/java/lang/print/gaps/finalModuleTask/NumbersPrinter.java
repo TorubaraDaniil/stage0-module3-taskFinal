df58280a1dd42b0d497fe819c224cb2ed63abe9b
daniil-torubara <dabarara@bk.ru>
2023-08-06 22:59:28 +0600

    module 3 final task

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -1,7 +1,8 @@
 package lang.print.gaps.finalModuleTask;
+import java.math.*;
 
 public class NumbersPrinter {
     public void printIsPositive(int number) {
-
+        System.out.println(number > 0);
     }
 }
